b2c4c8cbe6c429dec3c6cc445159532dc281b103
thellcishii-lang <the.llcishii@gmail.com>
2026-08-14 13:10:44 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -62,7 +62,7 @@ export default function PushTaroPage() {
       unsub();
       unsubMsg();
     };
-  }, []);
+  );
 
   const loadHistory = async () => {
     const all = await db.history.orderBy('sentAt').reverse().toArray();
