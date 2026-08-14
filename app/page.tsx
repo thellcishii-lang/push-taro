290b094f88846e45fa5e3d8c2b15e7d097305f52
thellcishii-lang <the.llcishii@gmail.com>
2026-08-14 09:51:40 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,8 @@ import {
   createUserWithEmailAndPassword,
   signOut,
 } from 'firebase/auth';
-import { auth } from '@/lib/firebase-client';
-import { requestFCMToken, onForegroundMessage } from '@/lib/firebase-client';
+import { auth } from '../lib/firebase-client';
+import { requestFCMToken, onForegroundMessage } from '../lib/firebase-client';
 import ImageUploader from '@/components/ImageUploader';
 import { db, exportHistoryToJSON, importHistoryFromJSON, PushHistory } from '@/lib/db';
 
