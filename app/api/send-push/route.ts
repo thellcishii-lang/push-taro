26acaaca2088f4cca4f53fd5fcc97262cee097ff
thellcishii-lang <the.llcishii@gmail.com>
2026-08-25 20:13:11 +0900

    Update route.ts

--- app/api/send-push/route.ts
+++ app/api/send-push/route.ts
@@ -85,6 +85,8 @@ export async function POST(request: Request) {
 
     // ✅ マルチキャスト送信
     const message = {
+      notification: {
+        title, body },
       data: {
         title: title,
         body: body,
