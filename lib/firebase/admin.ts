4197b5a4d41f719d72ba5f955c32c8c7e0528598
thellcishii-lang <the.llcishii@gmail.com>
2026-08-30 15:30:56 +0900

    Update admin.ts

--- lib/firebase/admin.ts
+++ lib/firebase/admin.ts
@@ -8,10 +8,11 @@ if (!admin.apps.length) {
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
     }),
-    // 🔥 これが重要！
-    firestore: {
-      ignoreUndefinedProperties: true,
-    },
+  });
+
+  // 🔥 firestore の設定は initializeApp の外で設定する！
+  admin.firestore().settings({
+    ignoreUndefinedProperties: true,
   });
 }
 
