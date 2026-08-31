3c330e79c808e427494697fcc66739a48fbbbfe9
thellcishii-lang <the.llcishii@gmail.com>
2026-08-31 09:58:13 +0900

    Update route.ts

--- app/api/system-admin/stats/route.ts
+++ app/api/system-admin/stats/route.ts
@@ -1,6 +1,6 @@
 // app/api/system-admin/stats/route.ts
 import { NextResponse } from 'next/server';
-import { db } from '../../../lib/firebase-admin';
+import { db } from '../../../../lib/firebase-admin';
 
 export async function GET(request: Request) {
   try {
