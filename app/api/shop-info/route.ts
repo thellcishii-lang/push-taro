2be22acffa8ca2d877abc5508bc8b8086b091c3e
thellcishii-lang <the.llcishii@gmail.com>
2026-08-31 09:47:10 +0900

    Update route.ts

--- app/api/shop-info/route.ts
+++ app/api/shop-info/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { db } from '../../../lib/firebase-admin';
+import { db } from '@/lib/firebase-admin';
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
