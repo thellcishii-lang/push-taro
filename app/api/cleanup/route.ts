d116e832e8330130390541d68b734854020974d4
thellcishii-lang <the.llcishii@gmail.com>
2026-08-31 09:44:49 +0900

    Update route.ts

--- app/api/cleanup/route.ts
+++ app/api/cleanup/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { messaging, db, authAdmin } from '../../../lib/firebase-admin';
+import { messaging, db, authAdmin } from '@/lib/firebase-admin';
 
 export async function POST(request: Request) {
   const authHeader = request.headers.get('Authorization');
