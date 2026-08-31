34279e3ecd2b2e0d101163498d6796622147882f
thellcishii-lang <the.llcishii@gmail.com>
2026-08-31 18:47:20 +0900

    Update route.ts

--- app/api/send-push/route.ts
+++ app/api/send-push/route.ts
@@ -1,5 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { adminAuth, adminDb, adminMessaging } from '../../../lib/firebase-admin';
+import { getApps, initializeApp, cert } from 'firebase-admin/app';
+import { getAuth } from 'firebase-admin/auth';
+import { getFirestore } from 'firebase-admin/firestore';
+import { getMessaging } from 'firebase-admin/messaging';
 
 export async function POST(req: NextRequest) {
   try {
