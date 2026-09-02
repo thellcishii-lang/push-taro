e5ffb510ff66e99c0e7a9254ed68134a2187dade
thellcishii-lang <the.llcishii@gmail.com>
2026-09-02 10:27:20 +0900

    Update page.tsx

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -10,7 +10,7 @@ import {
   PhoneAuthProvider,
   signInWithCredential,
 } from 'firebase/auth';
-import { app } from '@/lib/firebase-client';
+import { app } from '../../lib/firebase-client';
 
 const auth = getAuth(app);
 
