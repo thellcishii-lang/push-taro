4af5517fc2e6b21d6d3a8a8258e7fa9192127f97
thellcishii-lang <the.llcishii@gmail.com>
2026-09-09 09:42:00 +0900

    Update route.ts

--- app/api/admin/send-invoice/route.ts
+++ app/api/admin/send-invoice/route.ts
@@ -55,6 +55,10 @@ export async function POST(request: Request) {
         <p>決済完了後、自動的にサービスが再開されます。</p>
         <hr />
         <p><strong>Push-taro.com</strong></p>
+           <hr />
+            <p>運営会社：the合同会社</p>
+            <p>〒357-0123 埼玉県飯能市中藤下郷23-21</p>
+            <p><a href="mailto:pushtaro-info@gmail.com">pushtaro.info@gmail.com</a></p>
       `,
     });
 
