4c2f8f24ec5f9a605d64740d13078a6bf87623c9
thellcishii-lang <the.llcishii@gmail.com>
2026-09-11 08:37:36 +0900

    Update route.ts

--- app/api/manifest/route.ts
+++ app/api/manifest/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(request: Request) {
   const url = new URL(request.url);
   const shopId = url.searchParams.get('s') || '';
