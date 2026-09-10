2027c88586b1fd15a9dcd52259611bb7e7b5e807
thellcishii-lang <the.llcishii@gmail.com>
2026-09-11 08:42:11 +0900

    Update route.ts

--- app/api/system-admin/stats/route.ts
+++ app/api/system-admin/stats/route.ts
@@ -2,6 +2,8 @@
 import { NextResponse } from 'next/server';
 import { db } from '../../../../lib/firebase-admin';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(request: Request) {
   try {
     // 1. 全店舗データの取得
