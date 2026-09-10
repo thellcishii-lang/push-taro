0b5d7ee6a5a65033e9160577c0087d9e11c37915
thellcishii-lang <the.llcishii@gmail.com>
2026-09-11 08:42:49 +0900

    Update route.ts

--- app/api/unsubscribe/route.ts
+++ app/api/unsubscribe/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server';
 import { messaging, db } from '../../../lib/firebase-admin';
 
+export const dynamic = 'force-dynamic';
+
 export async function POST(request: Request) {
   try {
     const { token, shopId } = await request.json();
